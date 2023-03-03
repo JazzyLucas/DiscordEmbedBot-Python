4168d287d2e2cbdb878885f1a4416e1fef00c76e
JazzyLucas <59397873+JazzyLucas@users.noreply.github.com>
2023-03-02 19:57:30 -0500

    Update DiscordEmbedsBot.py

--- DiscordEmbedsBot.py
+++ DiscordEmbedsBot.py
@@ -24,8 +24,11 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_message(message):
-    # Message contains an iFunny link
+    # Check if the message contains an iFunny link
+
+    #iFunny
     if 'ifunny.co/' in message.content:
+        send_message = ":slight_smile: iFunny link from: " + message.author.mention + " :slight_smile:"
 
         # Extract the link from the message
         url = None
@@ -41,18 +44,22 @@ async def on_message(message):
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
             response = requests.get(url, headers=headers)
             html = response.text
+            send_message += "\nOriginal Link: <" + url + ">"
 
             # Parse the HTML and extract the video link
             soup = BeautifulSoup(html, 'html.parser')
             video_element = soup.find('video')
-            print("Found VideoElement.")
+            print(video_element)
             if video_element and 'data-src' in video_element.attrs:
                 video_link = video_element['data-src']
-                print("Sending video url: " + video_link)
-                await message.channel.send(video_link)
+                send_message += "\nVideo Link: " + video_link + ""
 
-    # Message contains an Twitter link
+            await message.channel.send(send_message)
+            await message.delete()
+
+    # Twitter
     if 'twitter.com/' in message.content:
+        send_message = ":bird: Twitter link from: " + message.author.mention + " :bird:"
 
         # Extract the link  from the message
         url = None
@@ -66,27 +73,28 @@ async def on_message(message):
             # Authenticate with the Twitter API
             auth = tweepy.OAuth2BearerHandler(TWITTER_BEARER_TOKEN)
             api = tweepy.API(auth)
+            send_message += "\nOriginal Link: <" + url + ">"
+
             # Get the tweet
             try:
                 match = re.search(r"status/(\d+)", url)
                 if match:
                     url = match.group(1)
-                    print("RegEx'ed tweet ID: " + url)
-                # Check if the tweet has a media attachment
+                # Check if the tweet has a video attachment
                 tweet = api.get_status(url, tweet_mode='extended')
                 media_entities = tweet.extended_entities.get('media', [])
-                # Sift through the media attachments for a video
                 for media in media_entities:
                     # Check if the media entity is a video
                     if 'video_info' in media:
-                        video_url = media['video_info']['variants'][0]['url']
-                        print("Sending video url: " + video_url)
-                        await message.channel.send(video_url)
-
+                        video_link = media['video_info']['variants'][0]['url']
+                        send_message += "\nVideo Link: " + video_link + ""
             except tweepy.errors.NotFound:
-                print("404 Tweet not found.")
+                send_message += "404 Tweet not found."
 
-    # Message contains an Twitter link
+            await message.channel.send(send_message)
+            await message.delete()
+
+    # Instagram
     if 'instagram.com/' in message.content:
 
         # Extract the link  from the message
@@ -103,6 +111,5 @@ async def on_message(message):
             # TODO
             # Get the tweet
             # TODO
-
 
 client.run(DISCORD_TOKEN)
